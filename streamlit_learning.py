b0e4b46f87bb77b799695de3c24cec1419e334d5
AnushaS0210 <160641494+AnushaS0210@users.noreply.github.com>
2024-02-22 21:42:30 +0530

    Update streamlit_learning.py
    2/22/2024 - 6

--- streamlit_learning.py
+++ streamlit_learning.py
@@ -10,6 +10,6 @@ st.write('Hello, *World!* :umbrella:')
 
 st.write(1234)
 st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40],
+    'First_Name': [Prerna,Nischay,Abhishek],
+    'Second_Name': [Malhan,Malhan,Malhan],
 }))
