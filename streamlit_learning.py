70767e1a3dfe99996a4b9cdd69d47b733081df11
AnushaS0210 <160641494+AnushaS0210@users.noreply.github.com>
2024-02-22 21:33:49 +0530

    Update streamlit_learning.py
    2/22/2024 - 2

--- streamlit_learning.py
+++ streamlit_learning.py
@@ -1,12 +1,12 @@
 import streamlit as at
 import pandas as pd
 
-st.write('Hello!')
+# st.write('Hello!')
 
 
-st.write('Hello, *World!* :sunglasses:')
+# st.write('Hello, *World!* :sunglasses:')
 
-st.write('Hello, *World!* :umbrella:')
+# st.write('Hello, *World!* :umbrella:')
 
 
 st.write(1234)
