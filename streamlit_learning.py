8dd509a867806e357c16b1245898581e8d055e02
AnushaS0210 <160641494+AnushaS0210@users.noreply.github.com>
2024-02-22 21:34:25 +0530

    Update streamlit_learning.py
    2/22/2024 - 3

--- streamlit_learning.py
+++ streamlit_learning.py
@@ -9,7 +9,7 @@ import pandas as pd
 # st.write('Hello, *World!* :umbrella:')
 
 
-st.write(1234)
+# st.write(1234)
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40],
